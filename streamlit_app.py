84d19ecee8c6de4337f8902a134f4ce1eb9df75a
gerywajana-ops <gerywajana@gmail.com>
2026-05-22 10:36:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
 st.title("🎈 Reyga")
+function addTask() {
+    let input = document.getElementById('taskInput');
+    let taskText = input.value;
+    if (taskText === '') return;
+
+    let li = document.createElement('li');
+    li.innerHTML = `${taskText} <button onclick="this.parentElement.remove()">X</button>`;
+    
+    document.getElementById('taskList').appendChild(li);
+    input.value = '';}
+
+
